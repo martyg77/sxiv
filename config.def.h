249f0f1a768e29835de4f1e751df5396f0e9aac2
Martin Gallant <martyg@goodbit.net>
2015-08-07 19:42:10 -0700

    Disable touchpad tap to click

--- config.def.h
+++ config.def.h
@@ -144,7 +144,7 @@ static const keymap_t keys[] = {
 /* mouse button mappings for image mode: */
 static const button_t buttons[] = {
 	/* modifiers    button            function              argument */
-	{ 0,            1,                i_navigate,           +1 },
+//	{ 0,            1,                i_navigate,           +1 },
 	{ 0,            3,                i_navigate,           -1 },
 	{ 0,            2,                i_drag,               None },
 	{ 0,            4,                i_scroll,             DIR_UP },
